34c69053d373223aa2cd58dfa24fb4dc734bca20
I. Dan Calinescu <idancalinescu@gmail.com>
2016-10-20 20:23:39 +0300

    added react containers

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ import App                from './lib/core/App'
 import React              from 'react'
 import { AppRegistry }    from 'react-native'
 
+// Allow the host app to start from this entry point
 export function startApp(props) {
   const Main = () => (<App {...props}/>)
   AppRegistry.registerComponent(props.name, () => Main)
 }
+
+// Make the common containers available to the host app
+export * from './lib/data/containers'
