d493d5e30d796bc85b4ef9f111bc8933ffaba4d8
I. Dan Calinescu <idancalinescu@gmail.com>
2016-10-21 03:29:02 +0300

    added token awareness to the auth op

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import App                from './lib/core/App'
 import React              from 'react'
-import { AppRegistry }    from 'react-native'
+import * as Config        from './lib/config'
+import * as Errors        from './lib/errors'
+import {
+  AppRegistry,
+  AsyncStorage
+} from 'react-native'
 
 // Allow the host app to start from this entry point
 export function startApp(props) {
@@ -19,3 +24,17 @@ export { default as RemoteOperation } from './lib/data/operations'
 
 // Allow host apps to access common configurations
 export * from './lib/config'
+
+// Give host apps the ability to fetch the token
+export function retrieveAuthToken() {
+  return new Promise((resolve, reject) => {
+    AsyncStorage.getItem(Config.AUTH_TOKEN_CACHE_KEY, (error, authToken) => {
+      if (error || !authToken) {
+        // The token was not found locally
+        reject(Errors.COULD_NOT_RETRIEVE_CACHED_TOKEN)
+        return
+      }
+      resolve(authToken)
+    })
+  })
+}
